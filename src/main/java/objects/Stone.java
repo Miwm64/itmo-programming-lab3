8acd67a9d02b27e53f9dade3584bc7905e5b052c
Miwm64 <mike.dobkes@gmail.com>
2025-12-03 02:38:51 +0300

    Stone implemented

---
+++ src/main/java/objects/Stone.java
@@ -0,0 +1,44 @@
+package objects;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Stone extends WorldObject implements Shineable{
+    // Constructors
+    public Stone(){
+        this("", LocalDateTime.now(), "");
+    }
+
+    public Stone(String title, LocalDateTime creationTime, String description){
+        super(title, creationTime, description);
+    }
+
+    @Override
+    public void shine(){
+        System.out.printf("Stone %s shines\n", title);
+    }
+
+    // Java object methods
+    @Override
+    public Stone clone() {
+        return (Stone) super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("type: Stone\ntitle: %s\ncreationTime: %s\ndescription: %s",
+                title, creationTime, description);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        return super.equals(o);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(getClass(), title, creationTime, description);
+    }
+}
