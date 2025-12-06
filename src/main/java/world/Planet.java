55ce7a3e4584a2565199c50a0ae97a434bffdb89
Miwm64 <mike.dobkes@gmail.com>
2025-12-06 15:49:31 +0300

    Java object methods implemented for Planet

--- src/main/java/world/Planet.java
+++ src/main/java/world/Planet.java
@@ -1,18 +1,27 @@
 package world;
 
+import objects.WorldObject;
+
 import java.util.Objects;
 
 public class Planet {
     protected WormholeStatus wormholeStatus;
     protected PlanetType planetType;
     protected Faction faction;
+    protected String title;
 
-    public Planet(WormholeStatus wormholeStatus, PlanetType planetType, Faction faction) {
+    public Planet(WormholeStatus wormholeStatus, PlanetType planetType,
+                  Faction faction, String title) {
         this.wormholeStatus = Objects.requireNonNull(wormholeStatus);
         this.planetType = Objects.requireNonNull(planetType);
         this.faction = Objects.requireNonNull(faction);
+        this.title = Objects.requireNonNull(title);
     }
 
+    // Setters
+    public void setTitle(String title) {
+        this.title = title;
+    }
     public void setPlanetType(PlanetType planetType) {
         this.planetType = planetType;
     }
@@ -25,6 +34,11 @@ public class Planet {
         this.faction = faction;
     }
 
+    // Getters
+    public String getTitle() {
+        return title;
+    }
+
     public PlanetType getPlanetType() {
         return planetType;
     }
@@ -35,5 +49,27 @@ public class Planet {
 
     public Faction getFaction() {
         return faction;
+    }
+
+    // Java object methods
+    @Override
+    public String toString() {
+        return String.format("type: Planet\ntitle: %s\nplanetType: %s\nfaction: %swormholeStatus: %s",
+                title, planetType, faction, wormholeStatus);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Planet p = (Planet) o;
+        return Objects.equals(title, p.title) && Objects.equals(planetType,
+                p.planetType) &&  Objects.equals(faction, p.faction)
+                &&  Objects.equals(wormholeStatus, p.wormholeStatus);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getClass(), title, planetType, faction, wormholeStatus);
     }
 }
