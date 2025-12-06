f2b323f97b241e13bce941fc4494cfb5179ccdbc
Miwm64 <mike.dobkes@gmail.com>
2025-12-06 15:36:29 +0300

    Planet getters and setters added

--- src/main/java/world/Planet.java
+++ src/main/java/world/Planet.java
@@ -1,7 +1,39 @@
 package world;
 
+import java.util.Objects;
+
 public class Planet {
     protected WormholeStatus wormholeStatus;
     protected PlanetType planetType;
     protected Faction faction;
+
+    public Planet(WormholeStatus wormholeStatus, PlanetType planetType, Faction faction) {
+        this.wormholeStatus = Objects.requireNonNull(wormholeStatus);
+        this.planetType = Objects.requireNonNull(planetType);
+        this.faction = Objects.requireNonNull(faction);
+    }
+
+    public void setPlanetType(PlanetType planetType) {
+        this.planetType = planetType;
+    }
+
+    public void setWormholeStatus(WormholeStatus wormholeStatus) {
+        this.wormholeStatus = wormholeStatus;
+    }
+
+    public void setFaction(Faction faction) {
+        this.faction = faction;
+    }
+
+    public PlanetType getPlanetType() {
+        return planetType;
+    }
+
+    public WormholeStatus getWormholeStatus() {
+        return wormholeStatus;
+    }
+
+    public Faction getFaction() {
+        return faction;
+    }
 }
